9ef49b8c21644469ded47ec728b8517f7c1f88b1
Mingcong Bai <jeffbai@aosc.io>
2025-09-30 00:42:23 +0800

    fix(components/ChildHeader): 修正返回上级页面的文字说明

--- components/ChildHeader.vue
+++ components/ChildHeader.vue
@@ -15,7 +15,7 @@
   <div class="back_row">
     <a :href="frontmatter.returnLink" class="btn_back">
       <svg class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" height="30"><path d="M206.336 512l264.832 264.832 60.330667-60.330667L326.997333 512l204.501334-204.501333-60.330667-60.330667L206.336 512z m241.066667 0l264.832 264.832 60.330666-60.330667L568.064 512l204.501333-204.501333-60.330666-60.330667L447.402667 512z" fill="#FF0000"></path></svg>
-      <span class="name">返回主页</span>
+      <span class="name">返回上级</span>
     </a>
   </div>
 
