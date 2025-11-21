90683bc42e1f8b9b460d13858242f92b723be4b0
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-11-21 17:15:06 +0800

    feat: refactor link components for improved readability and maintainability

---
+++ components/index/Link.vue
@@ -0,0 +1,23 @@
+<template>
+  <a :href="href" :target="isOutLink ? _blank : undefined">
+    <span class="name">{{ name }}</span>
+    <Icon v-if="isOutLink" icon="material-symbols:open-in-new" class="out-link-icon"/>
+  </a>
+</template>
+
+<script setup>
+import { Icon } from "@iconify/vue";
+
+const props = defineProps({
+  href: { type: String, required: true },
+  name: { type: String, required: true },
+});
+
+const isOutLink = props.href?.startsWith("http");
+</script>
+
+<style scoped>
+.out-link-icon {
+  margin-left: 2px;
+}
+</style>
