1811fcca228e563e85c4df02c6f049dd38c66e65
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-11-21 17:51:57 +0800

    feat: add some links with icons

--- components/index/Link.vue
+++ components/index/Link.vue
@@ -1,7 +1,12 @@
 <template>
-  <a :href="href" :target="isOutLink ? _blank : undefined">
+  <a :href="href" :target="target">
+    <Icon v-if="icon" :icon="icon" class="icon" />
     <span class="name">{{ name }}</span>
-    <Icon v-if="isOutLink" icon="material-symbols:open-in-new" class="out-link-icon"/>
+    <Icon
+      v-if="target"
+      icon="material-symbols:open-in-new"
+      class="out-link-icon"
+    />
   </a>
 </template>
 
@@ -11,13 +16,18 @@ import { Icon } from "@iconify/vue";
 const props = defineProps({
   href: { type: String, required: true },
   name: { type: String, required: true },
+  icon: String,
 });
 
-const isOutLink = props.href?.startsWith("http");
+const target = props.href?.startsWith("http") ? "_blank" : undefined;
 </script>
 
 <style scoped>
+.icon {
+  margin-right: 4px;
+}
+
 .out-link-icon {
-  margin-left: 2px;
+  margin-left: 4px;
 }
 </style>
