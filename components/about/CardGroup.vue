c4af3e7b63fe61fe06da0819a1d8dae4452535b7
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-11-25 01:11:34 +0800

    feat: add social links to the about page

---
+++ components/about/CardGroup.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="card-group">
+    <Card
+      name="GitHub 主页"
+      description="社区科技驱动的行星发电机"
+      href="https://github.com/loongson-community/1024"
+      icon="simple-icons:github"
+      color="#181717"
+    />
+    <Card
+      name="BiliBili"
+      description="关注最好的龙芯爱好者社区谢谢喵"
+      href="https://space.bilibili.com/70360929"
+      icon="simple-icons:bilibili"
+      color="#66ccff"
+    />
+    <Card
+      name="微信群"
+      description="常聊天！"
+      href="https://u.wechat.com/gPYFQQIJPEUImshdRvw1Prs?s=2"
+      icon="simple-icons:wechat"
+      color="#07C160"
+    />
+    <Card
+      name="QQ 群"
+      description="常联系！"
+      href="https://qm.qq.com/q/3oZByh8aOA"
+      icon="simple-icons:qq"
+      color="#1EBAFC"
+    />
+  </div>
+</template>
+
+<script setup>
+import Card from "./Card.vue";
+</script>
+
+<style scoped>
+.card-group {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 16px;
+}
+
+.card-group > * {
+  width: 100%;
+  margin-inline: auto;
+}
+
+@media (min-width: 40rem) {
+  .card-group {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media (min-width: 64rem) {
+  .card-group {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+@media (min-width: 80rem) {
+  .card-group {
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+</style>
--- pages/about.md
+++ pages/about.md
@@ -6,8 +6,12 @@ pageTitle: 关于社区
 pageSubTitle: 东方神秘第三方社区
 ---
 
+<script setup>
+import CardGroup from "../components/about/CardGroup.vue"
+</script>
 
 ![](/public/images/logo_about.webp)
 
 龙芯爱好者社区创立于 2024 年 10 月 24 日程序员节，是一个由第三方爱好者、行业人员与学生组成的，致力于龙芯（龙架构）软硬件生态建设的互联网社区。
 
+<CardGroup />
