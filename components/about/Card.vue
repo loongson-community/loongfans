720ea5fd411f738327dd7baea9eb34ff2666964d
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-11-25 01:11:34 +0800

    feat: add social links to the about page

---
+++ components/about/Card.vue
@@ -0,0 +1,48 @@
+<template>
+  <a :href="href">
+    <div>
+      <div class="name">{{ name }}</div>
+      <div class="description">{{ description }}</div>
+    </div>
+    <Icon :icon="icon" class="icon" height="24px" />
+  </a>
+</template>
+
+<script setup>
+import { Icon } from "@iconify/vue";
+
+defineProps({
+  name: String,
+  description: String,
+  href: String,
+  icon: String,
+  color: String,
+});
+</script>
+
+<style scoped>
+a {
+  display: flex;
+  justify-content: space-between;
+  padding: 16px;
+  color: unset;
+  text-decoration: unset;
+  border: 1px solid;
+  border-radius: 12px;
+  min-width: 260px;
+  max-width: 400px;
+}
+
+.icon {
+  color: v-bind(color);
+}
+
+.name {
+  font-size: large;
+}
+
+.description {
+  font-size: small;
+  color: #8d8d8d;
+}
+</style>
