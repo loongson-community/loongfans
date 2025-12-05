7ea76830b948e4440c955f8a70caa57a88c43e3f
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-12-05 20:45:26 +0800

    feat: setup PRIMEVUE

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,9 +1,15 @@
 import DefaultTheme from 'vitepress/theme'
-import Layout from './Layout.vue'
-import './custom.css'
+import PrimeVue from "primevue/config";
+import Material from "@primeuix/themes/aura";
+import { definePreset } from "@primeuix/themes";
 import { createI18n } from 'vue-i18n'
+
+import './custom.css'
 import en from '../locales/en.js'
 import zh from '../locales/zh.js'
+import Layout from './Layout.vue'
+
+import type { App } from "vue";
 
 const i18n = createI18n({
   legacy: false,
@@ -15,12 +21,32 @@ const i18n = createI18n({
   }
 })
 
+const Preset = definePreset(Material, {
+  semantic: {
+    primary: {
+      50: "{red.50}",
+      100: "{red.100}",
+      200: "{red.200}",
+      300: "{red.300}",
+      400: "{red.400}",
+      500: "#e60013",
+      600: "{red.600}",
+      700: "{red.700}",
+      800: "{red.800}",
+      900: "{red.900}",
+    },
+  },
+});
+
 export default {
   // 继承核心样式
   extends: DefaultTheme,
   // 覆盖布局但继承功能
-  Layout: Layout,
-  enhanceApp({ app }) {
+  Layout,
+  enhanceApp({ app }: { app: App }) {
     app.use(i18n)
-  }
-}
+    app.use(PrimeVue, {
+      theme: { preset: Preset, options: { darkModeSelector: false } },
+    });
+  },
+};
