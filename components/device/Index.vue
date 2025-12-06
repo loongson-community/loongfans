4ebd496f6e7ff8d822259bcbe2b1b14852fa24cc
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-12-06 16:54:04 +0800

    feat: use grids to display cards

--- components/device/Index.vue
+++ components/device/Index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <template v-for="family in devices" :key="family.family">
-      <h2>{{ family.family }}</h2>
-      <template v-for="category in family.categories" :key="category.title">
-        <h3>{{ category.title }}</h3>
+  <template v-for="family in devices" :key="family.family">
+    <h2>{{ family.family }}</h2>
+    <template v-for="category in family.categories" :key="category.title">
+      <h3>{{ category.title }}</h3>
+      <div class="card-grid">
         <Card
           v-for="device in category.devices"
           :key="device.href"
@@ -13,12 +13,27 @@
           :spec="device.spec"
           :tags="device.tags"
         />
-      </template>
+      </div>
     </template>
-  </div>
+  </template>
 </template>
 
 <script setup>
 import Card from "./Card.vue";
 import { devices } from "./data";
 </script>
+
+<style>
+.card-grid {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 16px;
+  margin-block: 16px;
+}
+
+@media (width >= 64rem) {
+  .card-grid {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+</style>
