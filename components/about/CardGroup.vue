395780f2df873dfa9a0578578a52e10324eea4c9
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-12-08 21:19:31 +0800

    feat: reroder cards for different languages in about page

--- components/about/CardGroup.vue
+++ components/about/CardGroup.vue
@@ -1,68 +1,79 @@
 <template>
   <div class="card-group">
     <Card
-      :name="t('aboutGithubName')"
-      :description="t('aboutGithubDescription')"
-      href="https://github.com/loongson-community/1024"
-      color="#181717"
+      v-for="item in cardDataMap[locale]"
+      :name="item.name"
+      :description="item.description"
+      :href="item.href"
+      :qr-link="item.qrLink"
+      :color="item.color"
     >
-      <IconGithub />
-    </Card>
-    <Card
-      :name="t('aboutBilibiliName')"
-      :description="t('aboutBilibiliDescription')"
-      href="https://space.bilibili.com/70360929"
-      color="#66ccff"
-    >
-      <IconBilibili />
-    </Card>
-    <Card
-      :name="t('aboutWechatName')"
-      :description="t('aboutWechatDescription')"
-      qr-link="/images/about/qr-wechat-group.png"
-      color="#07C160"
-    >
-      <IconWechat />
-    </Card>
-    <Card
-      :name="t('aboutQQName')"
-      :description="t('aboutQQDescription')"
-      qr-link="/images/about/qr-qq-group.png"
-      color="#1EBAFC"
-    >
-      <IconQq />
-    </Card>
-    <Card
-      :name="t('aboutMatrixName')"
-      :description="t('aboutMatrixDescription')"
-      href="https://matrix.to/#/#loongson-users:matrix.org"
-      color="#000000"
-    >
-      <IconMatrix />
-    </Card>
-    <Card
-      v-if="locale !== 'zh'"
-      :name="t('aboutTelegramName')"
-      :description="t('aboutTelegramDescription')"
-      href="https://t.me/loongson_users"
-      color="#26A5E4"
-    >
-      <IconTelegram />
+      <component :is="item.icon" />
     </Card>
   </div>
 </template>
 
 <script setup>
-import { useI18n } from 'vue-i18n';
+import { useI18n } from "vue-i18n";
 import Card from "./Card.vue";
 import IconGithub from "~icons/simple-icons/github";
 import IconBilibili from "~icons/simple-icons/bilibili";
 import IconWechat from "~icons/simple-icons/wechat";
 import IconQq from "~icons/simple-icons/qq";
 import IconMatrix from "~icons/simple-icons/matrix";
-import IconTelegram from "~icons/simple-icons/telegram"
+import IconTelegram from "~icons/simple-icons/telegram";
 
 const { t, locale } = useI18n();
+
+const cardData = {
+  github: {
+    name: t("aboutGithubName"),
+    description: t("aboutGithubDescription"),
+    href: "https://github.com/loongson-community/1024",
+    color: "#181717",
+    icon: IconGithub,
+  },
+  bilibili: {
+    name: t("aboutBilibiliName"),
+    description: t("aboutBilibiliDescription"),
+    href: "https://space.bilibili.com/70360929",
+    color: "#66ccff",
+    icon: IconBilibili,
+  },
+  wechat: {
+    name: t("aboutWechatName"),
+    description: t("aboutWechatDescription"),
+    qrLink: "/images/about/qr-wechat-group.png",
+    color: "#07C160",
+    icon: IconWechat,
+  },
+  qq: {
+    name: t("aboutQQName"),
+    description: t("aboutQQDescription"),
+    qrLink: "/images/about/qr-qq-group.png",
+    color: "#1EBAFC",
+    icon: IconQq,
+  },
+  matrix: {
+    name: t("aboutMatrixName"),
+    description: t("aboutMatrixDescription"),
+    href: "https://matrix.to/#/#loongson-users:matrix.org",
+    color: "#000000",
+    icon: IconMatrix,
+  },
+  telegram: {
+    name: t("aboutTelegramName"),
+    description: t("aboutTelegramDescription"),
+    href: "https://t.me/loongson_users",
+    color: "#26A5E4",
+    icon: IconTelegram,
+  },
+};
+
+const cardDataMap = {
+  zh: ["github", "bilibili", "wechat", "qq", "matrix"].map((i) => cardData[i]),
+  en: ["github", "bilibili", "telegram", "matrix", "wechat", "qq"].map((i) => cardData[i]),
+};
 </script>
 
 <style scoped>
