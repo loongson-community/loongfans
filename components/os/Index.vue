7eed842559e4530e1989763ecb463f20ad775356
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-12-26 02:32:21 +0800

    feat(os): add tags filter

--- components/os/Index.vue
+++ components/os/Index.vue
@@ -1,19 +1,56 @@
 <template>
-  <div class="distro-list">
-    <OsCard
-      v-for="card in $tm('osDataList')"
-      :key="card.name"
-      :data="card"
-    />
+  <div>
+    <div class="tags-filter">
+      <ToggleButton
+        v-for="tag in tagSet"
+        :key="tag"
+        v-model="selectedTags[tag]"
+        :on-label="tag"
+        :off-label="tag"
+        size="small"
+      />
+    </div>
+    <div class="os-list">
+      <OsCard v-for="card in filteredOsList" :key="card.name" :data="card" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import OsCard from './OsCard.vue'
+import { computed, reactive } from "vue";
+import { useI18n } from "vue-i18n";
+import ToggleButton from "primevue/togglebutton";
+import OsCard from "./OsCard.vue";
+import type { OsData } from "../../.vitepress/locales/zh/os";
+
+const { tm } = useI18n();
+
+const osDataList = computed(() => tm("osDataList") as OsData[]);
+const tagSet = computed(() => new Set(osDataList.value.flatMap((i) => i.tags)));
+const selectedTags = reactive<Record<string, boolean>>({});
+
+const filteredOsList = computed(() => {
+  const activeTags = new Set(
+    Object.keys(selectedTags).filter((t) => selectedTags[t])
+  );
+  return activeTags.size
+    ? osDataList.value.filter((os) => os.tags.some((t) => activeTags.has(t)))
+    : osDataList.value;
+});
 </script>
 
 <style scoped>
-.distro-list {
+.tags-filter {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+}
+
+.tags-filter > * {
+  height: 2em;
+}
+
+.os-list {
   display: flex;
   flex-direction: column;
   gap: 16px;
@@ -21,7 +58,7 @@ import OsCard from './OsCard.vue'
 }
 
 @media (width >= 48rem) {
-  .distro-list {
+  .os-list {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
   }
