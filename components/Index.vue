4f91a2e42495e618d6425aa2d8011f28d3cee2d7
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-12-27 16:59:36 +0800

    feat(index): use one column layout on narrow screens

--- components/Index.vue
+++ components/Index.vue
@@ -6,87 +6,51 @@
 
     <div class="site_nav_box">
       <div class="nav_list">
-        <div class="link_group">
-          <div class="link_type_title">
-            <div class="title">{{ t('beginnerResources') }}</div>
-            <div class="red_line"></div>
-          </div>
-          <div class="link_type_box">
-            <Link :href="`${basePath}/pages/intro`" :name="t('introToLoongson')" />
-            <Link :href="`${basePath}/pages/guides`" :name="t('usageGuides')" />
-            <Link :href="`${basePath}/pages/os`" :name="t('operatingSystems')" />
-            <Link :href="`${basePath}/pages/sdk`" :name="t('devGuides')" />
-          </div>
-        </div>
+        <LinkGroup :title="$t('beginnerResources')">
+          <Link
+            :href="`${basePath}/pages/intro`"
+            :name="t('introToLoongson')"
+          />
+          <Link :href="`${basePath}/pages/guides`" :name="t('usageGuides')" />
+          <Link :href="`${basePath}/pages/os`" :name="t('operatingSystems')" />
+          <Link :href="`${basePath}/pages/sdk`" :name="t('devGuides')" />
+        </LinkGroup>
 
-        <div class="link_group">
-          <div class="link_type_title">
-            <div class="title">{{ t('supportMaterials') }}</div>
-            <div class="red_line"></div>
-          </div>
-          <div class="link_type_box">
-            <Link :href="`${basePath}/pages/chips`" :name="t('chipsData')" />
-            <Link :href="`${basePath}/pages/devices`" :name="t('productSpecs')" />
-            <Link href="https://loong123.cn/" :name="t('compatibilityDb')" />
-          </div>
-        </div>
+        <LinkGroup :title="$t('supportMaterials')">
+          <Link :href="`${basePath}/pages/chips`" :name="t('chipsData')" />
+          <Link :href="`${basePath}/pages/devices`" :name="t('productSpecs')" />
+          <Link href="https://loong123.cn/" :name="t('compatibilityDb')" />
+        </LinkGroup>
 
-        <div class="link_group">
-          <div class="link_type_title">
-            <div class="title">{{ t('contact') }}</div>
-            <div class="red_line"></div>
-          </div>
-          <div class="link_type_box">
-
-            <Link :name="t('aboutBilibiliName')" href="https://space.bilibili.com/70360929">
-              <IconBilibili />
-            </Link>
-            <Link :name="t('aboutWechatName')" qr-link="/images/about/qr-wechat-group.png">
-              <IconWechat />
-            </Link>
-            <Link :name="t('aboutQQName')" qr-link="/images/about/qr-qq-group.png">
-              <IconQq />
-            </Link>
-            <Link :name="t('aboutMatrixName')" href="https://matrix.to/#/#loongson-users:matrix.org">
-              <IconMatrix />
-            </Link>
-            <Link v-if="locale !== 'zh'" :name="t('aboutTelegramName')" href="https://t.me/loongson_users">
-              <IconTelegram />
-            </Link>
-          </div>
-        </div>
+        <LinkGroupContact class="link-group-1" />
       </div>
 
       <div class="nav_list">
-        <div class="link_group">
-          <div class="link_type_title">
-            <div class="title">{{ t('communityResources') }}</div>
-            <div class="red_line"></div>
-          </div>
-          <div class="link_type_box">
-            <Link :href="`${basePath}/pages/biweekly`" :name="t('biweeklyMeeting')" />
-            <Link :href="`${basePath}/pages/jobs`" :name="t('jobsAndBounties')" />
-            <Link
-              :name="t('aboutGithubName')"
-              href="https://github.com/loongson-community"
-            />
-            <Link href="https://github.com/loongson-community/1024" :name="t('devBoardProgram')" />
-            <Link href="https://www.loongbbs.cn/" :name="t('communityForum')" />
-            <Link href="https://areweloongyet.com/" :name="t('areWeLoongYet')" />
-          </div>
-        </div>
+        <LinkGroup :title="$t('communityResources')">
+          <Link
+            :href="`${basePath}/pages/biweekly`"
+            :name="t('biweeklyMeeting')"
+          />
+          <Link :href="`${basePath}/pages/jobs`" :name="t('jobsAndBounties')" />
+          <Link
+            :name="t('aboutGithubName')"
+            href="https://github.com/loongson-community"
+          />
+          <Link
+            href="https://github.com/loongson-community/1024"
+            :name="t('devBoardProgram')"
+          />
+          <Link href="https://www.loongbbs.cn/" :name="t('communityForum')" />
+          <Link href="https://areweloongyet.com/" :name="t('areWeLoongYet')" />
+        </LinkGroup>
 
-        <div class="link_group">
-          <div class="link_type_title">
-            <div class="title">{{ t('loongsonOfficial') }}</div>
-            <div class="red_line"></div>
-          </div>
-          <div class="link_type_box">
-            <Link href="https://www.loongson.cn/" :name="t('loongsonTech')" />
-            <Link href="https://www.loongeco.cn/" :name="t('loongsonEco')" />
-            <Link href="https://app.loongapps.cn/" :name="t('loongApps')" />
-          </div>
-        </div>
+        <LinkGroup :title="$t('communityResources')">
+          <Link href="https://www.loongson.cn/" :name="t('loongsonTech')" />
+          <Link href="https://www.loongeco.cn/" :name="t('loongsonEco')" />
+          <Link href="https://app.loongapps.cn/" :name="t('loongApps')" />
+        </LinkGroup>
+
+        <LinkGroupContact class="link-group-2" />
       </div>
     </div>
   </div>
@@ -94,15 +58,12 @@
 
 <script setup>
 import { computed } from "vue";
-import { useData } from 'vitepress';
-import IconBilibili from "~icons/simple-icons/bilibili";
-import IconWechat from "~icons/simple-icons/wechat";
-import IconQq from "~icons/simple-icons/qq";
-import IconMatrix from "~icons/simple-icons/matrix";
-import IconTelegram from "~icons/simple-icons/telegram"
+import { useData } from "vitepress";
 import Link from "./index/Link.vue";
-import { useI18n } from 'vue-i18n';
-import { getLocalePrefix } from '../.vitepress/utils/language';
+import LinkGroup from "./index/LinkGroup.vue";
+import LinkGroupContact from "./index/LinkGroupContact.vue";
+import { useI18n } from "vue-i18n";
+import { getLocalePrefix } from "../.vitepress/utils/language";
 
 const { t, locale } = useI18n();
 const { localeIndex } = useData();
@@ -159,86 +120,12 @@ body {
   height: auto;
 }
 
-.link_type_title {
+.link-group-1 {
   display: block;
-  clear: both;
-  width: 100%;
-  height: auto;
-  overflow: hidden;
-  position: relative;
-  cursor: default;
 }
 
-.link_type_title .title {
-  display: block;
-  clear: both;
-  width: 100%;
-  overflow: hidden;
-  font-size: 24px;
-  font-weight: bold;
-  color: #e60013;
-  margin-bottom: 4px;
-  line-height: normal;
-}
-
-.link_type_title .red_line {
-  display: block;
-  clear: both;
-  width: 100%;
-  height: 5px;
-  overflow: hidden;
-  background-color: #e60013;
-}
-
-.link_group {
-  padding: 0;
-}
-
-/* .link_group:hover .title {
-  font-size: 26px;
-  transition: font-size 0.5s ease 0s;
-}
-
-.link_group:hover .red_line {
-  height: 0;
-  transition: height 0.5s ease 0s;
-} */
-
-.link_type_box {
-  display: block;
-  clear: both;
-  width: 100%;
-  height: auto;
-  overflow: visible;
-  padding: 20px 0px;
-}
-
-.link_type_box > * {
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  clear: both;
-  width: 100%;
-  min-height: 40px;
-  height: auto;
-  line-height: normal;
-  overflow: visible;
-  font-size: 22px;
-  color: #000000;
-  text-decoration: none;
-  padding: 5px 0;
-  word-wrap: break-word;
-}
-
-.link_type_box > * .name {
-  word-wrap: break-word;
-  overflow-wrap: break-word;
-  white-space: normal;
-}
-
-.link_type_box > *:hover {
-  font-weight: bold;
-  color: #e60013;
+.link-group-2 {
+  display: none;
 }
 /*站点导航清单-结束*/
 
@@ -333,5 +220,19 @@ body {
   }
 }
 
+@media (max-width: 450px) {
+  .site_nav_box {
+    flex-wrap: wrap;
+    gap: 0;
+  }
+
+  .link-group-1 {
+    display: none;
+  }
+
+  .link-group-2 {
+    display: block;
+  }
+}
 /*响应式处理-结束*/
 </style>
