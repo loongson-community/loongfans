a33e65042b11eb83b8f80d9e00008b0152845919
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-12-27 16:59:36 +0800

    feat(index): use one column layout on narrow screens

---
+++ components/index/LinkGroup.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="link_group">
+    <div class="link_type_title">
+      <div class="title">{{ title }}</div>
+      <div class="red_line"></div>
+    </div>
+    <div class="link_type_box">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ title: string }>();
+</script>
+
+<style scoped>
+.link_type_title {
+  display: block;
+  clear: both;
+  width: 100%;
+  height: auto;
+  overflow: hidden;
+  position: relative;
+  cursor: default;
+}
+
+.link_type_title .title {
+  display: block;
+  clear: both;
+  width: 100%;
+  overflow: hidden;
+  font-size: 24px;
+  font-weight: bold;
+  color: #e60013;
+  margin-bottom: 4px;
+  line-height: normal;
+}
+
+.link_type_title .red_line {
+  display: block;
+  clear: both;
+  width: 100%;
+  height: 5px;
+  overflow: hidden;
+  background-color: #e60013;
+}
+
+.link_group {
+  padding: 0;
+}
+
+/* .link_group:hover .title {
+  font-size: 26px;
+  transition: font-size 0.5s ease 0s;
+}
+
+.link_group:hover .red_line {
+  height: 0;
+  transition: height 0.5s ease 0s;
+} */
+
+.link_type_box {
+  display: block;
+  clear: both;
+  width: 100%;
+  height: auto;
+  overflow: visible;
+  padding: 20px 0px;
+}
+
+.link_type_box > * {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  clear: both;
+  width: 100%;
+  min-height: 40px;
+  height: auto;
+  line-height: normal;
+  overflow: visible;
+  font-size: 22px;
+  color: #000000;
+  text-decoration: none;
+  padding: 5px 0;
+  word-wrap: break-word;
+}
+
+.link_type_box > * .name {
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+  white-space: normal;
+}
+
+.link_type_box > *:hover {
+  font-weight: bold;
+  color: #e60013;
+}
+</style>
---
+++ components/index/LinkGroupContact.vue
@@ -0,0 +1,45 @@
+<template>
+  <LinkGroup :title="$t('contact')">
+    <Link
+      :name="$t('aboutBilibiliName')"
+      href="https://space.bilibili.com/70360929"
+    >
+      <IconBilibili />
+    </Link>
+    <Link
+      :name="$t('aboutWechatName')"
+      qr-link="/images/about/qr-wechat-group.png"
+    >
+      <IconWechat />
+    </Link>
+    <Link :name="$t('aboutQQName')" qr-link="/images/about/qr-qq-group.png">
+      <IconQq />
+    </Link>
+    <Link
+      :name="$t('aboutMatrixName')"
+      href="https://matrix.to/#/#loongson-users:matrix.org"
+    >
+      <IconMatrix />
+    </Link>
+    <Link
+      v-if="locale !== 'zh'"
+      :name="$t('aboutTelegramName')"
+      href="https://t.me/loongson_users"
+    >
+      <IconTelegram />
+    </Link>
+  </LinkGroup>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+import IconBilibili from "~icons/simple-icons/bilibili";
+import IconWechat from "~icons/simple-icons/wechat";
+import IconQq from "~icons/simple-icons/qq";
+import IconMatrix from "~icons/simple-icons/matrix";
+import IconTelegram from "~icons/simple-icons/telegram";
+import Link from "./Link.vue";
+import LinkGroup from "./LinkGroup.vue";
+
+const { locale } = useI18n();
+</script>
