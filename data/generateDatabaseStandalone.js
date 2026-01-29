7ec9dc84d79a2974330d56472a86cfcbf48b644a
LiarOnce <liaronce@hotmail.com>
2026-01-29 22:45:09 +0800

    fix: run prettier

--- data/generateDatabaseStandalone.js
+++ data/generateDatabaseStandalone.js
@@ -1,7 +1,7 @@
 // This script is intended for use without relying on VitePress,
 // specifically for testing JSON files independently.
 
-import { generateAll } from "../.vitepress/plugins/loongfans-data/generateDatabase";
+import { generateAll } from "../.vitepress/plugins/loongfans-data/generateDatabase"
 
 // Default: Generating everyone without formatted data
 // If require formatted JSON files, set this function value to 1.
