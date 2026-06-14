8580990e1888dfd97429aa89f4985b086cec8753
WANG Xuerui <git@xen0n.name>
2026-06-14 23:40:54 +0800

    refactor(index): reuse social link data for LinkGroupContact

--- src/client/components/index/LinkGroupContact.vue
+++ src/client/components/index/LinkGroupContact.vue
@@ -1,48 +1,42 @@
 <template>
   <LinkGroup :title="$t('contact')">
     <HomePageLink
-      :name="$t('aboutBilibiliName')"
-      href="https://space.bilibili.com/70360929"
+      v-for="link in linksData"
+      :key="link.name"
+      :name="link.name"
+      :href="link.url"
+      :qr-link="link.qrcodeURL"
     >
-      <IconBilibili />
-    </HomePageLink>
-    <HomePageLink
-      :name="$t('aboutWechatName')"
-      qr-link="/images/about/qr-wechat-group.png"
-    >
-      <IconWechat />
-    </HomePageLink>
-    <HomePageLink
-      :name="$t('aboutQQName')"
-      qr-link="/images/about/qr-qq-group.png"
-    >
-      <IconQq />
-    </HomePageLink>
-    <HomePageLink
-      :name="$t('aboutMatrixName')"
-      href="https://matrix.to/#/#loongson-users:matrix.org"
-    >
-      <IconMatrix />
-    </HomePageLink>
-    <HomePageLink
-      v-if="locale !== 'zh'"
-      :name="$t('aboutTelegramName')"
-      href="https://t.me/loongson_users"
-    >
-      <IconTelegram />
+      <component :is="link.icon" />
     </HomePageLink>
   </LinkGroup>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue"
 import { useI18n } from "vue-i18n"
-import IconBilibili from "~icons/simple-icons/bilibili"
-import IconWechat from "~icons/simple-icons/wechat"
-import IconQq from "~icons/simple-icons/qq"
-import IconMatrix from "~icons/simple-icons/matrix"
-import IconTelegram from "~icons/simple-icons/telegram"
+
+import {
+  getSocialLinksForLocale,
+  type SocialLinkDisplayConfig,
+} from "@src/client/utils/socialLinks"
 import HomePageLink from "./HomePageLink.vue"
 import LinkGroup from "./LinkGroup.vue"
 
-const { locale } = useI18n()
+const { locale, t } = useI18n()
+
+const displayConfig: SocialLinkDisplayConfig = {
+  zh: ["bilibili", "wechat", "qq", "matrix"],
+  "": ["bilibili", "wechat", "qq", "matrix", "telegram"],
+}
+
+const linksData = computed(() => {
+  const links = getSocialLinksForLocale(locale.value, displayConfig)
+  return links.map((link) => ({
+    name: t(link.nameKey),
+    url: link.url,
+    qrcodeURL: link.qrcodeURL,
+    icon: link.icon,
+  }))
+})
 </script>
