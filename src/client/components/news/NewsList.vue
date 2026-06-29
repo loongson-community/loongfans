b9a3ee61c79a78431da7adde9d8995196e988a9f
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2026-06-29 16:35:35 +0800

    fix(news): load entries with locale

--- src/client/components/news/NewsList.vue
+++ src/client/components/news/NewsList.vue
@@ -1,6 +1,6 @@
 <template>
   <ul>
-    <li v-for="item in data[category].slice(0, limit)" :key="item.url">
+    <li v-for="item in filteredData" :key="item.url">
       <a :href="`${basePath}${item.url}`">
         {{ item.frontmatter.pageTitle }} |
         {{ item.frontmatter.pageSubTitle }}
@@ -15,9 +15,19 @@ import { useData } from "vitepress"
 import { getLocalePrefix } from "@src/client/utils/language"
 // @ts-expect-error 'data' export is handled by VitePress
 import { data } from "@src/node/data-loaders/news.data"
+import type { ContentData } from "vitepress"
 
-withDefaults(defineProps<{ category: string; limit?: number }>(), { limit: 10 })
+const props = withDefaults(
+  defineProps<{ category: string; limit?: number }>(),
+  { limit: 10 },
+)
 
 const { localeIndex } = useData()
 const basePath = computed(() => getLocalePrefix(localeIndex.value))
+const filteredData = data
+  .filter(
+    (item: ContentData & { localeIndex: string; category: string }) =>
+      item.localeIndex == localeIndex.value && item.category == props.category,
+  )
+  .slice(0, props.limit)
 </script>
