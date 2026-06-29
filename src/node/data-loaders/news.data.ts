9a8ef2073eb9513fe9fc6d4b6f6b7f670cb17de9
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2026-06-29 16:35:35 +0800

    fix(news): load entries with locale

--- src/node/data-loaders/news.data.ts
+++ src/node/data-loaders/news.data.ts
@@ -1,8 +1,14 @@
 import { createContentLoader } from "vitepress"
-export default createContentLoader("news/**/*.md", {
+export default createContentLoader("**/news/**/*.md", {
   includeSrc: false,
   render: false,
   excerpt: false,
   transform: (data) =>
-    Object.groupBy(data, (item) => item.url.split("/").at(-2) || "others"),
+    data.map((item) => ({
+      ...item,
+      category: item.url.split("/").at(-2) || "others",
+      localeIndex: item.url.startsWith("/news")
+        ? "root"
+        : item.url.split("/")[1],
+    })),
 })
