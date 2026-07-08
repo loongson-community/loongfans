2182dcfd48519c3b96f49a6bb6a597b9bf06de91
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2026-07-08 16:30:43 +0800

    refactor(zh): add a fallback page for environments without JS

---
+++ pages/index.md
@@ -0,0 +1,9 @@
+---
+layout: home
+---
+
+<script setup lang="ts">
+import IndexFallback from "@src/client/components/IndexFallback.vue"
+</script>
+
+<IndexFallback />
---
+++ src/client/components/IndexFallback.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex justify-center my-16 gap-16 flex-wrap sm:flex-nowrap">
+    <div class="w-[400px]">
+      <img src="/en/images/logo.webp" />
+    </div>
+    <LinkGroup title="Select Language">
+      <HomePageLink
+        v-for="language in SupportedLanguage"
+        :key="language"
+        :href="getLocaleUrl(language, router.route.path)"
+        :name="LANGUAGE_DISPLAY_NAMES[language]"
+      />
+    </LinkGroup>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vitepress"
+import {
+  getLocaleUrl,
+  LANGUAGE_DISPLAY_NAMES,
+} from "@src/client/utils/language"
+import { SupportedLanguage } from "@src/types/language"
+import LinkGroup from "./index/LinkGroup.vue"
+import HomePageLink from "./index/HomePageLink.vue"
+
+const router = useRouter()
+</script>
