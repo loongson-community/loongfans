9c8bd7f4c4e1c80e8d6fbf4ad6ad7484a1f92d3f
WANG Xuerui <git@xen0n.name>
2026-07-24 15:10:54 -0300

    Reapply "build: bump to VitePress 2.0.0-alpha.18 and Vite 8"
    This reverts commit d6954a11786375d65f9e0f628cb2f7e4ed05891e.

--- scripts/pnpm-exec-wrapper.sh
+++ scripts/pnpm-exec-wrapper.sh
@@ -7,15 +7,24 @@ _running_on_loong64() {
   [[ $( uname -m ) == loongarch64 ]]
 }
 
-_has_loong64_binding_pkg() {
-  [[ $(pnpm why @oxc-parser/binding-linux-loong64-gnu 2>&1 | wc -l) -gt 0 ]]
+_napi_binding_pkgs_to_check=(
+  @oxc-parser/binding-linux-loong64-gnu
+  @rolldown/binding-linux-loong64-gnu
+)
+
+_pnpm_has_pkg() {
+  [[ $(pnpm why "$1" 2>&1 | wc -l) -gt 0 ]]
 }
 
 main() {
-  if _running_on_loong64 && ! _has_loong64_binding_pkg; then
-    echo "note: @oxc-parser/binding-linux-loong64-gnu seems not existing yet" >&2
-    echo "note: force-enabling napi-rs WASI fallback codepath" >&2
-    export NAPI_RS_FORCE_WASI=1
+  if _running_on_loong64; then
+    for pkg in "${_napi_binding_pkgs_to_check[@]}"; do
+      _pnpm_has_pkg "$pkg" && continue
+      echo "note: $pkg seems not existing yet" >&2
+      echo "note: force-enabling napi-rs WASI fallback codepath" >&2
+      export NAPI_RS_FORCE_WASI=1
+      break
+    done
   fi
   exec pnpm exec "$@"
 }
